03bee050999024ac3f5f472fbc74511d0efe830b
Joe Sembler <joeysembler@gmail.com>
2021-12-19 18:13:44 -0500

    commit tree 147de9a03aded9d49b41ed188fdb3252198b1f8f

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  // const technologiesSpan = technologies.map(() => {
+  //   technologies.array.forEach(element => {
+  //     return <span>{element}</span>;
+  //   });
+  // });
+
+  console.log(technologies);
+  const technologiesItems = technologies.map((technology) => {
+    return <span>{technology}</span>;
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologiesItems}
       </div>
     </div>
   );
